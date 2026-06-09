2159418460e28a81ec0e47563fc46dd824ec49dc
Richard Ruiz <richardruizs1998@gmail.com>
2026-06-09 17:17:41 -0600

    Add Gestor project files

---
+++ Gestor/Controllers/ClienteController.cs
@@ -0,0 +1,95 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestor.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ClienteController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync("https://localhost:7217/api/cliente");
+            if (response.IsSuccessStatusCode)
+            {
+                var clientes = await response.Content.ReadFromJsonAsync<List<API.Models.Cliente>>();
+                return View(clientes);
+            }
+            else
+            {
+                return View(new List<API.Models.Cliente>());
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Cliente cleinte)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            await httpClient.PostAsJsonAsync("https://localhost:7217/api/cliente", cleinte);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync($"https://localhost:7217/api/cliente/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var cliente = await response.Content.ReadFromJsonAsync<API.Models.Cliente>();
+                return View(cliente);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Cliente cliente)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            await httpClient.PutAsJsonAsync($"https://localhost:7217/api/cliente/{id}", cliente);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            await httpClient.DeleteAsync($"https://localhost:7217/api/cliente/{id}");
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync($"https://localhost:7217/api/cliente/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var cliente = await response.Content.ReadFromJsonAsync<API.Models.Cliente>();
+                return View(cliente);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+
+
+
+
+        }
+}
---
+++ Gestor/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Identificacion { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string Direccion { get; set; } = string.Empty;
+
+        public ICollection<Telefono> Telefonos { get; set; } = new List<Telefono>();
+    
+    }
+}
+
