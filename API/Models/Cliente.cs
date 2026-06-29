77f3ddd737d0b6e89abc16553fb39c55ddcb66db
Zick <cihc2006@gmail.com>
2026-06-28 23:08:20 -0600

    Update to Client API
    Se actualizaron los modelos de Cliente y Telefono con las validaciones correspondientes. Se agrego confirmacion de fecha de nacimiuento para validar que sea mayor de edad y se valida que la identificacion no se repita.

--- API/Models/Cliente.cs
+++ API/Models/Cliente.cs
@@ -1,16 +1,32 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
 {
     public class Cliente
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nombre { get; set; } = string.Empty;
+
+        [Required]
         public string Apellido { get; set; } = string.Empty;
+
+        [Required]
         public string Identificacion { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
         public string Correo { get; set; } = string.Empty;
+
         public string Direccion { get; set; } = string.Empty;
 
+        [Required]
+        public DateOnly fechaNacimiento { get; set; }
+        
         public ICollection<Telefono> Telefonos { get; set; } = new List<Telefono>();
     
     }
+
 }
 
--- API/Models/Telefono.cs
+++ API/Models/Telefono.cs
@@ -1,12 +1,21 @@
-﻿namespace API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
 {
     public class Telefono
     {
+        
         public int Id { get; set; }
+
+        [Required]
         public string Numero { get; set; } = string.Empty;
+
+        [Required]
         public string Tipo { get; set; } = string.Empty;
 
+        [Required]
         public int ClienteId { get; set; }
+
         public Cliente? Cliente { get; set; }
     }
 }
